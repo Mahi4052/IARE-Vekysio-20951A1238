85fa2325266dee974d86f8c70b4a19b5f49c97ad
Mahi4052 <95848665+Mahi4052@users.noreply.github.com>
2023-03-03 20:55:32 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,56 @@
-t=int(input())
-for i in range(t):
-  y,m,z,x=map(int,input().split())
-  try:
-    print((y*m)/(z*x))
-  execpt:
-    print("Don't enter zeros and negatives")
-    
+#Using recursion
+
+public class Main {
+    public static void main(String[] args) {
+        String paragraph = "Hello Recursion";
+        System.out.println(reverseParagraph(paragraph));
+    }
+
+    private static String reverseParagraph(String paragraph) {
+        String[] words = paragraph.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (String word : words) {
+            sb.append(reverseWord(word)).append(" ");
+        }
+        return sb.toString();
+    }
+
+    private static String reverseWord(String word) {
+        if (word.length() == 0) {
+            return "";
+        } else {
+            return word.charAt(word.length() - 1) + reverseWord(word.substring(0, word.length() - 1));
+        }
+    }
+}
+
+#Using multithreading
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ReverseWordsInParagraph {
+    public static void main(String[] args) {
+        String paragraph = "Hello Multithereading";
+        String[] words = paragraph.split("\\s+");
+
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        for (String word : words) {
+            executor.execute(() -> System.out.print(reverseWord(word) + " "));
+        }
+        executor.shutdown();
+    }
+
+
+
+    private static String reverseWord(String word) {
+        if (word.length() == 0) {
+            return "";
+        } else {
+            return word.charAt(word.length() - 1) + reverseWord(word.substring(0, word.length() - 1));
+        }
+    }
+}
+
     
   
